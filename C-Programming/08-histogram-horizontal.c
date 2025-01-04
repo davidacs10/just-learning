a6eecff2ce426332b725d398d1deaff1a91e90b4
davidacs10 <davidacs10.08@gmail.com>
2025-01-04 06:31:41 -0400

    08-histogram-horizontal.c

---
+++ C-Programming/08-histogram-horizontal.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main()
+{
+    int state;
+    int length, max;
+    int count[999];
+    int c, i, j;
+
+    state = OUT;
+    length = max = 0;
+    for (i = 0; i < 999; ++i)
+    {
+        count[i] = 0;
+    }
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ' || c == '\n' || c == '\t')
+        {
+            state = OUT;
+            if (length > max)
+            {
+                max = length;
+            }
+            if (length != 0)
+            {
+                count[length]++;
+            }
+            length = 0;
+        }
+        else if (state == OUT)
+        {
+            state = IN;
+            length++;
+        }
+        else
+            length++;
+    }
+    for (i = 0; i < max + 1; ++i)
+    {
+        printf("%2d: ", i);
+        for (j = 0; j < count[i]; ++j)
+        {
+            putchar('#');
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
